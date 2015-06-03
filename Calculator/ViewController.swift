33287647bfc6033a587a642646a1da11d0b1ab4a
lvshuwei <183331215@qq.com>
2015-06-03 11:18:30 +0800

    edit some code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         }else{
             display.text=digit
             userIsInTheMiddleOfTypingANumber=true
+            
         }
         
     }
