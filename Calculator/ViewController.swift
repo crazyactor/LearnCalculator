23db13e3dd831bff6b68efa1b5def3fc2be6817d
lvshuwei <183331215@qq.com>
2015-07-04 17:33:11 +0800

    a little change

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var userIsInTheMiddleOfTypingANumber:Bool = false
     
+    var brain=CalculatorBrain()
+    
     @IBAction func appendDigit(sender: UIButton){
         let digit=sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber{
@@ -26,31 +28,21 @@ class ViewController: UIViewController {
         
     }
     
+
     @IBAction func operate(sender: UIButton) {
-        let operation=sender.currentTitle!
+    
         if userIsInTheMiddleOfTypingANumber{
         enter()
         }
-        switch operation{
-            case "√":performOperation{sqrt($0)}
-            case "÷":performOperation{$1/$0}
-            case "+":performOperation{$1+$0}
-            case "−":performOperation{$1-$0}
-            default:break
+        if let operation=sender.currentTitle{
+            if let result=brain.performOperation(operation){
+                displayValue=result
+            }else{
+                displayValue=0
+            }
         }
         
-    }
-    func performOperation(operation:(Double,Double)->Double){
-        if operandStack.count>=2{
-            displayValue=operation(operandStack.removeLast(),operandStack.removeLast())
-            enter()
-        }
-    }
-    private func performOperation(operation:Double->Double){
-        if operandStack.count>=1{
-            displayValue=operation(operandStack.removeLast())
-            enter()
-        }
+        
     }
     
     var operandStack=Array<Double>()
@@ -59,8 +51,11 @@ class ViewController: UIViewController {
 
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber=false
-        operandStack.append(displayValue)
-        println("operandStack=\(operandStack)")
+        if let result=brain.pushOperand(displayValue){
+        displayValue=result
+        }else{
+            displayValue=0
+        }
     }
 
     var displayValue:Double{
